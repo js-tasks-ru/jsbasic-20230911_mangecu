016079190a886588f8ad1a1a4270f802f13c4a36
Ragimov Fuad <foyado@gmail.com>
2023-09-14 00:43:43 +0300

    Выполнил все задания. Все тесты пройдены.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  switch (n) {
+    case '0':
+    case '1':
+      return 1
+    default:
+      let i = 0;
+      let result = 1;
+      while (i < n) {
+        result = result * (n - i);
+        i++;
+      }
+      return result
+  }
 }
+
