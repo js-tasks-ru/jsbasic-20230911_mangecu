362f3dc628768babfadfcba9a63e1b807eca63de
Ragimov Fuad <foyado@gmail.com>
2023-10-03 01:41:21 +0300

    Из ДЗ3, ДЗ4 и ДЗ5 все задания сделаны. Все тесты пройдены

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  table.querySelectorAll('tbody tr').forEach(elem => {
+
+    switch (elem.cells[3].dataset.available) {
+      case "true":
+        elem.classList.add('available');
+        break;
+      case "false":
+        elem.classList.add('unavailable');
+        break;
+      default:
+        elem.hidden = 'true';
+    }
+
+    switch (elem.cells[2].textContent) {
+      case "m":
+        elem.classList.add('male');
+        break;
+      case "f":
+        elem.classList.add('female');
+        break;
+    }
+
+    if (elem.cells[1].textContent < 18) {
+      elem.style.textDecoration = 'line-through';
+    }
+  })
 }
