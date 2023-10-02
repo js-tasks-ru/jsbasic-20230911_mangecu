714b54d22bc17df8c528d76ac29d2966f894d5c1
Ragimov Fuad <foyado@gmail.com>
2023-10-03 01:32:04 +0300

    Из ДЗ3, ДЗ4 и ДЗ5 все задания сделаны. Все тесты пройдены

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector('.carousel__inner');
+  let carouselArrow = document.querySelectorAll('.carousel__arrow');
+  let carouselSlides = document.querySelectorAll('.carousel__slide');
+  let count= 0;
+  let widthShift = carousel.offsetWidth;
+  carouselArrow[1].style.display = 'none'
+
+  carouselArrow.forEach(button => {
+    button.addEventListener('click',event => {
+      if (event.currentTarget.classList.contains('carousel__arrow_right')) {
+        count = count - 1;
+      } else if (event.currentTarget.classList.contains('carousel__arrow_left')) {
+        count = count + 1;
+      }
+      carousel.style.transform = `translateX(${count * widthShift}px)`;
+      hiddenArrow (count, carouselArrow);
+    })
+  })
+  function hiddenArrow (count, carouselArrow) {
+    switch (count) {
+      case 0:
+        carouselArrow[1].style.display = 'none';
+        break
+      case -3:
+        carouselArrow[0].style.display = 'none';
+        break
+      default:
+        carouselArrow[0].style.display = 'flex';
+        carouselArrow[1].style.display = 'flex';
+    }
+  }
 }
